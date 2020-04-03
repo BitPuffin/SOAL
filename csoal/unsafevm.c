f611c81c885f411d229030fe8b3013c639ee9e14
Isak <contact@bitpuffin.com>
2020-04-03 14:30:08 +0900

    make the instruction dispatch a bit more sophisticated

--- csoal/unsafevm.c
+++ csoal/unsafevm.c
@@ -21,13 +21,28 @@ enum regcode {
 	REG_13,
 	REG_14,
 	REG_15,
+
+	REG_SP, /* stack pointer */
+	REG_SBP, /* stack base pointer */
+
 	REG_COUNT,
+};
+
+
+enum operand_mode {
+	MODE_REG,
+	MODE_MEM,
+};
+
+struct operand {
+	u8 mode;
+	u8 reg;
 };
 
 struct instruction {
 	u8 opcode;
-	enum regcode reg_a;
-	enum regcode reg_b;
+	u8 operand_count;
+	struct operand operands[4];
 };
 
 struct unsafevm {
@@ -38,17 +53,25 @@ struct unsafevm {
 	struct instruction *iptr;
 };
 
+/* data given during dispatch to instruction */
+struct instruction_data {
+	void *a;
+	void *b;
+	void *c;
+	void *d;
+};
 
-
-void op_add_int(struct unsafevm *vm, struct instruction in)
+struct op_add_data {
+	int *src;
+	int *dest;
+};
+void op_add_int(struct unsafevm *vm, struct instruction_data *id)
 {
-	printf("running add instruction\n");
-	int a = vm->registers[in.reg_a];
-	int b = vm->registers[in.reg_b];
-	vm->registers[in.reg_b] = a + b;
+	struct op_add_data *opd = (struct op_add_data *)id;
+	*opd->dest = *opd->src + *opd->dest;
 }
 
-typedef void (*instruction_impl)(struct unsafevm *, struct instruction);
+typedef void (*instruction_impl)(struct unsafevm *, struct instruction_data *);
 enum opcode {
 	OP_CALL,
 	OP_LEAVE,
@@ -65,20 +88,37 @@ instruction_impl op_impls[] = {
 	NULL,
 };
 
-void advance_program(struct unsafevm *vm)
+void advance_instruction(struct unsafevm *vm)
 {
-	struct instruction in = *vm->iptr;
-	vm->iptr++;
-	op_impls[in.opcode](vm, in);
-}
+	struct instruction *in = vm->iptr;
+	size_t operand_offset = in->operand_count * sizeof(struct operand);
+	vm->iptr += (sizeof(struct instruction) + operand_offset);
 
+	struct instruction_data data = {};
+	void **oprdata = (void *)&data;
+	struct operand *opr = in->operands;
+	void *end = opr + in->operand_count;
+	for (; opr < end; opr++) {
+		switch(opr->mode) {
+		case MODE_REG:
+			*oprdata = &vm->registers[opr->reg];
+			break;
+		case MODE_MEM:
+			*oprdata = (void *)vm->registers[opr->reg];
+			break;
+		}
+		oprdata++;
+	}
+
+	op_impls[in->opcode](vm, &data);
+}
 
 void test_add() {
 	struct unsafevm vm = {};
 	struct instruction add = {
 		OP_ADD_INT,
-		REG_0,
-		REG_2
+		2,
+		{ { MODE_REG, REG_0 }, { MODE_REG, REG_2 } }
 	};
 	vm.registers[REG_0] = 15;
 	vm.registers[REG_2] = 17;
@@ -88,14 +128,9 @@ void test_add() {
 	       (int) vm.registers[REG_0],
 	       (int) vm.registers[REG_2]);
 
-	advance_program(&vm);
+	advance_instruction(&vm);
 
 	printf("after:  {r0: %d} {r2: %d}\n",
 	       (int) vm.registers[REG_0],
 	       (int) vm.registers[REG_2]);
 }
-
-
-
-
-
